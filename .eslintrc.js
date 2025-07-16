cea739961da9ca10994069a01f29ff69fc6cb4a0
Jarif Junaeed <shin_secure@proton.me>
2025-07-16 06:48:20 +0000

    chore: Add config files for style enforce and package.json
    * chore: Add .eslintrc.js
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Add .prettierrc
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Add tsconfig.json
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: create .prettierignore
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Remove browser: true from .eslintrc.js and rename .eslintrc.cjs
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: rename .prettierrc to .prettierrc.cjs
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Update tsconfig.json
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * docs: fix broken links and formatting errors in CONTRIBUTING.md
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: update .eslintrc.cjs to .eslintrc.js using ESM
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Changed .prettierrc.cjs to .prettierrc.js using ESM
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Update tsconfig.json to use ESM instead of cjs
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    * chore: Add package.json
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>
    
    ---------
    
    Signed-off-by: Jarif Junaeed <shin_secure@proton.me>

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+export default {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json', // Point to your tsconfig.json file
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    // You can add your own custom rules here
+  },
+};
